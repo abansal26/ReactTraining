f8bd0c3eff5b573febab5ff689d95780c3f389d6
abansal26 <abansal@grepruby.com>
2017-12-13 17:30:18 +0530

    Added materia;-ui functions and some styling

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import request from 'request';
-import Table from './table.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CircularProgress from 'material-ui/CircularProgress';
+import List from './table.jsx';
 
 export default class App extends React.Component {
   constructor() {
@@ -23,8 +25,21 @@ export default class App extends React.Component {
   
   render() {
     return (
-      <div>
-        <Table list={this.state.list} />
+      <div style={{height: "97%"}}>
+        <MuiThemeProvider>
+          {this.state.list ? 
+            <List list={this.state.list} /> :
+            <CircularProgress 
+              size={80}
+              thickness={5}
+              style={{
+                display:"flex",
+                justifyContent:"center",
+                alignItems:"center",
+                width: "100%",
+                height: "100%"}}/>
+          }
+        </MuiThemeProvider>
       </div>
     )
   }
