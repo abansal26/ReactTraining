4de59315019f5c89d89e1bbcb2c7316526aef297
abansal26 <abansal@grepruby.com>
2017-12-18 18:41:51 +0530

    Converted Main to function type from class type

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -9,33 +9,29 @@ import { RaisedButton } from 'material-ui/';
 import Home from './home.jsx';
 import ToDo from './todo.jsx';
 
-export default class Main extends React.Component {
-  constructor() {
-    super()
-  }
-
-  render() {
-    return (
-      <div> 
-        <RaisedButton
-          label='HOME'
-          primary={true}
-          containerElement={
-            <Link to='/' />
-          }
-        />
-        <RaisedButton
-          label='TODO'
-          secondary={true}
-          containerElement={
-            <Link to='/todo' />
-          }
-        />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/todo' component={ToDo} />
-        </Switch>
-      </div>
-    )
-  }
+const Main = () => {
+  return (
+    <div> 
+      <RaisedButton
+        label='HOME'
+        primary={true}
+        containerElement={
+          <Link to='/' />
+        }
+      />
+      <RaisedButton
+        label='TODO'
+        secondary={true}
+        containerElement={
+          <Link to='/todo' />
+        }
+      />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/todo' component={ToDo} />
+      </Switch>
+    </div>
+  )
 }
+
+export default Main;
